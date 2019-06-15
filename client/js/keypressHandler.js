c7b04d9e652f582adaf9f4a79624d57ad0dcf3f4
kataing <kataing12@gmail.com>
2019-06-15 12:34:39 -0700

    Implements client side keypresses

--- client/js/keypressHandler.js
+++ client/js/keypressHandler.js
@@ -1,10 +1,28 @@
-
 $('body').on('keydown', (event) => {
-  var arrowPress = event.key.match(/Arrow(Up|Down|Left|Right)/);
-  if (arrowPress) {
-    var direction = arrowPress[1];
-    SwimTeam.move(direction.toLowerCase());
-  }
-});
+    var arrowPress = event.key.match(/Arrow(Up|Down|Left|Right)/);
+    if (arrowPress) {
+      var direction = arrowPress[1];
+      postSwimCommand(direction);
+      // SwimTeam.move(direction.toLowerCase());
+    }
+  });
 
 console.log('Client is running in the browser!');
+
+
+const postSwimCommand = (direction) => {
+  console.log('init');
+  $.ajax({
+    type: 'POST',
+    url: 'http://127.0.0.1:3000',
+    data: direction,
+    error: () => {console.log('YOU ERRORED')},
+    success: (data) => {
+      // var actions = data.split(',');
+      // for(var i = 0; i < actions.length; i++) {
+      //   SwimTeam.move(actions[i]);
+      // }
+      console.log('data', data);
+    }
+  });
+}
